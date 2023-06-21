be36b3d39de2e8cafabbdb1c8450bab1cc6ef747
rajeshmandava <rajesh.mandava07@gmail.com>
2023-06-21 22:31:45 +0200

    First go with react components

---
+++ arrayOfComponents/app.js
@@ -0,0 +1,13 @@
+let cards = data.map((course,i)=>{
+    return (<Card key={i} data={course}/>)
+})
+
+console.log(cards)
+
+ReactDOM.render(
+    <div className="row"> 
+        {cards}
+       
+        </div>,
+        document.getElementById('root')
+)
