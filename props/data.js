991941fb4248b9b965c430c21d1970596d11947a
rajeshmandava <rajesh.mandava07@gmail.com>
2023-06-21 07:05:04 +0200

    Trying components and props

---
+++ props/data.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        course : "React From the Beginning",
+        instructor  : "Rajesh Mandava"
+    },
+    {
+        course : "Apache Kafka Series",
+        instructor : "Stephane Maarek"
+    },
+    {
+        course : "Music Production in Logic Pro X",
+        instructor : "Tomas George"
+    },
+    {
+        course : "Unity Game Development",
+        instructor : "Jonathan Wienberger"
+    }
+]
