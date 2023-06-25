c9f55915ca723639505ffb440c8cd0491e043e94
rajeshmandava <rajesh.mandava07@gmail.com>
2023-06-25 17:46:09 +0200

    States in React first go

---
+++ section2/src/StateInAction.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class StateInAction extends Component{
+    constructor(){
+        super();
+        this.state={
+            text: "State In Action"
+        }
+        setTimeout(()=>{
+            this.setState({
+                text: "State Changed"
+            })
+        },2000)
+    }
+    
+    render()
+    {
+        return(
+            <h1>{this.state.text}</h1>
+        )
+    }
+}
+
+export default StateInAction;
