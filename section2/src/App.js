8dbe4f9cc4796b1c652e7294ab05d3109a01a543
rajeshmandava <rajesh.mandava07@gmail.com>
2023-06-25 23:54:31 +0200

    State and props together in React

--- section2/src/App.js
+++ section2/src/App.js
@@ -4,13 +4,20 @@ import React, {Component} from 'react';
 // import StateInAction from './StateInAction'
 // import SimpleEvents from './SimpleEvents'
 // import EventAndState from './SimpleEvents';
-import StatePractice from './StatePractice';
+// import StatePractice from './StatePractice';
+import CardSet from './CardSet';
+import cards from './cards'
+
+// console.log(cards)
 
 class App extends Component {
   render(){
   return (
     <div className="App">
-      <StatePractice/>
+      <div className="row">
+      <CardSet cards={cards}/>
+        
+        </div>
     </div>
   );
 }
---
+++ section2/src/CardSet.js
@@ -0,0 +1,57 @@
+import React,{Component} from 'react';
+
+class CardSet extends Component{
+    
+    constructor(){
+        super();
+        this.state={
+            chosenCards:[]
+        }
+    }
+    
+    saveCourse=(index)=>{
+        console.log(index)
+        
+        const copyOfCards = [...this.state.chosenCards];
+        copyOfCards.push(this.props.cards[index])
+        this.setState({
+            chosenCards:copyOfCards
+        })
+    }
+    
+    render(){
+        console.log(this.state.chosenCards)
+
+        const savedCards = this.state.chosenCards.map((card)=>{
+            return <h3>{card.course}</h3>
+        })
+        const cardList = this.props.cards.map((card,i)=>{
+            return(
+                <div className="col s2" key={i}>
+                    <div className="card hoverable small">
+                    <div className="card-image">
+                    <img src={card.image} />
+                </div>
+                <div className="card-content">
+                    <p>{card.course}</p>
+                    <p>{card.instructor}</p>
+                </div>
+                <div className="card-action">
+                    <a href="#">$9.99</a>
+                    </div>
+                </div>
+                <button  onClick={()=>{this.saveCourse(i)}} className="btn waves-light waves-effect">Save</button>
+            </div>	
+            )
+        })
+
+        return(
+            <div>
+                {cardList}
+                {savedCards}
+            </div>
+        )
+    }
+}
+
+export default CardSet;
