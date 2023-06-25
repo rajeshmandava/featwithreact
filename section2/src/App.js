71d77bac97642885f2890d1ec63124938f26b0a3
rajeshmandava <rajesh.mandava07@gmail.com>
2023-06-25 23:26:23 +0200

    Practice states in react

--- section2/src/App.js
+++ section2/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import React, {Component} from 'react';
 // import StateInAction from './StateInAction'
 // import SimpleEvents from './SimpleEvents'
-import EventAndState from './SimpleEvents';
+// import EventAndState from './SimpleEvents';
+import StatePractice from './StatePractice';
 
 class App extends Component {
   render(){
   return (
     <div className="App">
-      <EventAndState/>
+      <StatePractice/>
     </div>
   );
 }
