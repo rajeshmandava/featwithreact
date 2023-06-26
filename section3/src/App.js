7c10450a35dbebba43ea09f8a2945413811d038b
rajeshmandava <rajesh.mandava07@gmail.com>
2023-06-26 23:42:42 +0200

    Component lifecycle in React

--- section3/src/App.js
+++ section3/src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+  constructor(){
+    super();  
+    this.state={
+      temp:""
+    }
+  }
+
+  componentDidMount(){
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d'
+    console.log("calling render again");
+    axios.get(url).then((resp)=>{
+      this.setState({
+        temp:resp.data.main.temp
+      })
+    })
+
+  }
+
+  render(){
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{this.state.temp}</h1>
     </div>
   );
+}
 }
 
 export default App;
